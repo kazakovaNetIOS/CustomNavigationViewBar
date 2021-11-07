b5590aae999a0593cf87bef0ddda63cfe461d01e
Kazakova Natalia Vladimirovna <natkazakova@ozon.ru>
2021-11-07 13:15:19 +0300

    Initial Commit

---
+++ CustomNavigationViewBar/CustomNavigationViewBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomNavigationViewBarApp.swift
+//  CustomNavigationViewBar
+//
+//  Created by Kazakova Nataliya on 07.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct CustomNavigationViewBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
